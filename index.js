03f5612bc24ede29abda67d71848ef754ebd1b8e
skooger <adamjskooglambda@gmail.com>
2020-02-13 10:33:53 -0800

    fixed a typo

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ server.get("/", (req,res) => {
 })
 
 //make the port dynamic
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 server.listen(port, () => console.log(`Your server is running on ${port}`))
